a526823e732e9c256a2dca539a5e3018d16223a7
Samson Olaide <121801210+SOO2023@users.noreply.github.com>
2025-04-05 17:27:11 +0100

    db setup (#5)
    Co-authored-by: Samson Olaide <samson.olaide@kodehauz.com>

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Optional
 from pydantic_settings import BaseSettings
 from pydantic import AnyHttpUrl
+from pydantic import field_validator, PostgresDsn, ValidationInfo
 
 
 class Settings(BaseSettings):
@@ -15,6 +16,27 @@ class Settings(BaseSettings):
     BASE_FILE_PATH: str = "uploads"
 
     USE_SQLITE: bool = False
+    SQLITE_URI: Optional[str]
+
+    POSTGRES_USER: Optional[str] = None
+    POSTGRES_PASSWORD: Optional[str] = None
+    POSTGRES_SERVER: Optional[str] = None
+    POSTGRES_DB: Optional[str] = None
+
+    POSTGRES_URI: Optional[PostgresDsn] = None
+
+    @field_validator("POSTGRES_URI", mode="before")
+    @classmethod
+    def posgres_uri(cls, v: Optional[str], info: ValidationInfo):
+        user = info.data.get("POSTGRES_USER")
+        password = info.data.get("POSTGRES_PASSWORD")
+        server = info.data.get("POSTGRES_SERVER")
+        db = info.data.get("POSTGRES_DB")
+
+        if v:
+            return v
+        elif all([user, password, server, db]):
+            return f"postgresql://{user}:{password}@{server}/{db}"
 
     class Config:
         env_file = ".env"
---
+++ app/db/session.py
@@ -0,0 +1,11 @@
+from sqlmodel import create_engine
+from app.core import settings
+
+if settings.USE_SQLITE:
+    engine = create_engine(
+        url=settings.SQLITE_URI,
+        connect_args={"check_same_thread": False},
+        pool_pre_ping=True,
+    )
+else:
+    engine = create_engine(url=settings.POSGRES_URI, pool_pre_ping=True)
