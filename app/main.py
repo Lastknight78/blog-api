93166db419ec4e4711cb5b9a8767fcd75925c201
Samson Olaide <121801210+SOO2023@users.noreply.github.com>
2025-04-05 16:01:45 +0100

    set up app and configurations (#4)
    Co-authored-by: Samson Olaide <samson.olaide@kodehauz.com>

--- app/main.py
+++ app/main.py
@@ -1,4 +1,26 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from app.core import settings
 
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.PROJECT_VERSION,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+)
 
-app = FastAPI()
+if settings.CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+# app.include_router()
+
+if settings.STORAGE_TYPE == "file":
+    app.mount(
+        settings.BASE_FILE_PATH, StaticFiles(directory=settings.BASE_FILE_PATH), name="storage"
+    )
